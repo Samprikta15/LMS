a985118a818d32fc818f63d13233aeba5ef18e69
Samprikta15 <samprikta1507@gmail.com>
2024-05-01 09:42:24 +0530

    IN ADMIN PANEL CHANGED ATRIBUTES NAME

--- register/Registration_frontend.php
+++ register/Registration_frontend.php
@@ -133,21 +133,21 @@
     <a href="http://localhost/LMS/home/index.php">Home</a>
 </div>
 <script>
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+  /* When the user clicks on the button, 
+  toggle between hiding and showing the dropdown content */
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
     }
   }
-}
 
 </script>
     <div class="container">
